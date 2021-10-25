f6e354e05ddf2c7fb119e2453cfa2fa56422635c
pushittoprod <31714132+PushItToProd@users.noreply.github.com>
2021-10-25 06:17:15 -0700

    create EnvArgs struct

--- main.go
+++ main.go
@@ -5,29 +5,78 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 )
 
-func main() {
-	bind_addr := os.Getenv("BIND_ADDR")
-	if bind_addr == "" {
-		bind_addr = ":8080"
+type EnvArgs struct {
+	bindAddr string
+	symbol   string
+	ndays    int
+	apikey   string
+}
+
+func getEnvArgs() *EnvArgs {
+	bindAddr := os.Getenv("BIND_ADDR")
+	if bindAddr == "" {
+		bindAddr = ":8080"
 	}
+
 	symbol := os.Getenv("SYMBOL")
 	if symbol == "" {
 		panic("The SYMBOL environment variable must be set")
 	}
-	ndays := os.Getenv("NDAYS")
-	if ndays == "" {
+
+	ndays_str := os.Getenv("NDAYS")
+	if ndays_str == "" {
 		panic("The NDAYS environment variable must be set")
 	}
+	ndays, err := strconv.Atoi(ndays_str)
+	if err != nil {
+		fmt.Errorf("Invalid value for NDAYS: %v", ndays)
+		panic("The NDAYS environment variable must be a valid integer")
+	}
+
 	apikey := os.Getenv("APIKEY")
 	if apikey == "" {
 		panic("The APIKEY environment variable must be set")
 	}
 
-	log.Printf("Starting web server on %s", bind_addr)
+	return &EnvArgs{bindAddr: bindAddr, symbol: symbol, ndays: ndays, apikey: apikey}
+}
+
+type ApiResponse struct {
+	symbol  string
+	data    []float64 // in real code, I would not use float64 for currency
+	average float64
+}
+
+func createApiResponse(symbol string, ndays int) *ApiResponse {
+	data := []float64{110.56, 111.25, 115.78}
+	average := mean(data)
+	return &ApiResponse{
+		symbol:  symbol,
+		data:    data,
+		average: average,
+	}
+}
+
+func mean(xs []float64) float64 {
+	sum := 0.0
+	for _, x := range xs {
+		sum += x
+	}
+	return sum / float64(len(xs))
+}
+
+func main() {
+	args := getEnvArgs()
+
+	log.Printf("Starting web server on %s", args.bindAddr)
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "symbol=%s, ndays=%s", symbol, ndays)
+		data := createApiResponse(args.symbol, args.ndays)
+		fmt.Fprintf(w, "%s, data=%v, average=%.2f", data.symbol, data.data, data.average)
 	})
-	log.Fatal(http.ListenAndServe(bind_addr, nil))
+
+	log.Fatal(http.ListenAndServe(args.bindAddr, nil))
 }
